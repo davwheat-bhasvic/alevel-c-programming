d6457fd1f9ed67d569f3bf8ff79a15794f7c7693
David Wheatley <hi@davwheat.dev>
2020-09-18 09:50:59 +0100

    Revert "Remove CMake folder"
    This reverts commit f82e68a40b653922ca69fcc42906049c2701798d.

---
+++ Chapter 1 - Intro to C/cmake-build-debug/CMakeFiles/Chapter_1___Intro_to_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter_1___Intro_to_C.dir/main.c.obj"
+  "Chapter_1___Intro_to_C.exe"
+  "Chapter_1___Intro_to_C.exe.manifest"
+  "Chapter_1___Intro_to_C.lib"
+  "Chapter_1___Intro_to_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chapter_1___Intro_to_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
