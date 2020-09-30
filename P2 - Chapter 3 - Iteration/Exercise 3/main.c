5b489c5b1371b587475523579cf04309370857be
David Wheatley <hi@davwheat.dev>
2020-09-30 11:04:07 +0100

    P2C3: add exercise 3

---
+++ P2 - Chapter 3 - Iteration/Exercise 3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+
+    printf("A magic number is one which is divisible by 7 or 3, and, when multiplied by 5, is below 100.\n");
+
+    do {
+        printf("\nEnter a number to check if it's MaGiCaL (or 100 to quit): ");
+        scanf("%i", &num);
+
+        int isMagic = 1;
+
+        // If not a multiple of 7 or 3, it's not magic
+        if (num % 7 != 0 && num % 3 != 0) isMagic = 0;
+
+        // If >= 100 when multiplied by 5, it's not magic
+        if (num * 5 >= 100) isMagic = 0;
+
+        if (isMagic)
+            printf("It is MaGiCaL!");
+        else
+            printf("It's not magical.");
+    } while (num != 100);
+
+    return 0;
+}
