af311debec3e6f092a7808d6e5cf99b4ed9bc7c3
David Wheatley <hi@davwheat.dev>
2020-09-30 10:47:08 +0100

    P2C3: add exercise 1

---
+++ P2 - Chapter 3 - Iteration/Exercise 1/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define MAX_NUMBER 10
+#define ATTEMPTS_AVAILABLE 5
+
+int main() {
+    // initialise randomizer
+    srand(time(NULL));
+
+    int randomNumber = (rand() % MAX_NUMBER) + 1,
+            pGuess = 0,
+            guessesRemaining = ATTEMPTS_AVAILABLE;
+
+    while (guessesRemaining > 0) {
+        printf("Guess my random number (1-%d): ", MAX_NUMBER);
+        scanf("%d", &pGuess);
+
+        if (pGuess == randomNumber) {
+            printf("Congratulations!\n");
+            break;
+        } else {
+            if (pGuess > randomNumber)
+                printf("Your guess was too high!\n");
+            else
+                printf("Your guess was too low!\n");
+
+            guessesRemaining -= 1;
+        }
+    }
+
+    if (guessesRemaining == 0) {
+        printf("\nYou've run out of guesses. You lose!");
+    }
+
+    printf("\nThe number was %d", randomNumber);
+
+    return 0;
+}
