669679c766002de034e860096e3e9fb6a6146793
David Wheatley <davidwheatley03@gmail.com>
2020-10-09 09:56:04 +0100

    P2C4: add wip extension task 2

---
+++ P2 - Chapter 4 - Arrays/Extension Task 2/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 25
+
+int main() {
+    // Init randomiser, should only be called once.
+    srand(time(NULL));
+
+    char words[MAX_STRING_LENGTH][10] = {"elephant", "barbaric", "sanitise", "virus", "infectious"};
+    int score = 0;
+
+    for (int i = 0; i < 10; i++) {
+        int len = strlen(words[i]);
+        int missingIndex = rand() % len;
+
+        // create temp array for the output
+        char output[MAX_STRING_LENGTH];
+        strcpy(output, words[i]);
+
+        // hide char at the random index
+        output[missingIndex] = '?';
+
+        printf("%s", output);
+        printf("\nWhat's the full word? ");
+
+        char input[MAX_STRING_LENGTH];
+        fgets(input, MAX_STRING_LENGTH, stdin);
+
+        printf("in=%s", input);
+        printf("og=%s\n", words[i]);
+
+        if (strcmp(input, words[i]) == 0) {
+            printf("Correct! The word was %s\n\n", words[i]);
+            score++;
+        } else {
+            printf("Incorrect. The word was %s\n\n", words[i]);
+        }
+    }
+
+    return 0;
+}
