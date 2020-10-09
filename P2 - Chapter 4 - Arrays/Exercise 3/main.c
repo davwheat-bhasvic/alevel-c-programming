4ef747ec542a108c6489de2b672c01da5f4ac7e8
David Wheatley <davidwheatley03@gmail.com>
2020-10-09 09:30:26 +0100

    P2C5: add exercise 3

---
+++ P2 - Chapter 4 - Arrays/Exercise 3/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+// length of pre-set word
+#define LENGTH 6
+
+int main() {
+    char word[] = "dinner";
+    char guess[] = "??????";
+    int guessCount = 1;
+
+    while (strcmp(word, guess) != 0 && guessCount <= 5) {
+        char input[LENGTH + 1];
+
+        printf("%s", guess);
+        printf("\nGuess the word:");
+        // gets is bad! >:(
+        fgets(input, LENGTH + 1, stdin);
+
+        for (int i = 0; i < LENGTH; i++) {
+            // if letter in guess matches letter in word...
+            if (input[i] == word[i]) {
+                // ...overwrite the letter in the guess variable
+                guess[i] = input[i];
+            }
+        }
+
+        guessCount++;
+    }
+    if (guessCount > 5) {
+        printf("You lose!");
+    } else {
+        printf("You win!");
+    }
+
+}
