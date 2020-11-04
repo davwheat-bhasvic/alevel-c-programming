51bca82eeee9bc07a2289e6a5a3520ac66ebe5db
David Wheatley <hi@davwheat.dev>
2020-11-04 10:48:16 +0000

    P2C5: add exercise 1

---
+++ P2 - Chapter 5 - Subroutines/Exercise 1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int divEleven(int number);
+
+int main() {
+    puts("Enter a number to divide by 11.");
+
+    int number;
+    scanf_s("%i", &number);
+
+    printf("%d", divEleven(number));
+
+    return 0;
+}
+
+int divEleven(const int number) {
+    return number / 11;
+}
